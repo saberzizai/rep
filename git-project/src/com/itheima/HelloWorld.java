f190295580f1485b19e0d78dc25928255d2ef4b5
zhangsan <zhangsan@itcast.cn>
2019-09-20 20:43:03 +0800

    第一次提交代码

---
+++ git-project/src/com/itheima/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("你好啊 我来了 别跑啊");
+        System.out.println("hello 我来了..");
+
+        System.out.println("我是老菜鸟");
+
+        System.out.println("我的dev 分支....");
+    }
+}
